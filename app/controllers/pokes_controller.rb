bddced084be3ae166e36a81a61680f65fb04a96e
Andrew Braga <andrewbraga1@gmail.com>
2021-06-22 18:32:39 -0300

    *pokes* * adicionado tratamento de rotas e de casos iniciais

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -1,18 +1,34 @@
 class PokesController < ApplicationController
+    before_action :poke_params
     def busca 
-        params.permit(:pokemon_name)
-        begin
-            response = RestClient.get "https://pokeapi.co/api/v2/pokemon/"+params[:pokemon_name], {accept: :json}
-            response_json = JSON.parse response
-            res = response_json["abilities"].map{ |item| item["ability"]["name"]} if response_json["abilities"].size > 0
-        rescue
-            res = 'Sem resultados'
+        
+        if params[:pokemon_name].present?
+            begin
+                response = RestClient.get "https://pokeapi.co/api/v2/pokemon/"+params[:pokemon_name], {accept: :json}
+                response_json = JSON.parse response
+                res = response_json["abilities"].map{ |item| item["ability"]["name"]} if response_json["abilities"].size > 0
+            rescue
+                res = 'Sem resultados'
+            end
+        else
+            res = 'Digite um nome válido'
         end
-      
         respond_to do |format|    
             format.html { render json: res, status:200 }
             format.json { render json: res, status:200 }
         end
+    end
+
+    def no_valid
+        res = 'Digite um nome válido'
+        respond_to do |format|    
+            format.html { render json: res, status:200 }
+            format.json { render json: res, status:200 }
+        end
+    end
+
+    def poke_params
+        params.permit(:pokemon_name)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "busca-pokemon/:pokemon_name", to: "pokes#busca"
+  get "busca-pokemon/", to: "pokes#no_valid"
   resources :pokes do
   end
 end
